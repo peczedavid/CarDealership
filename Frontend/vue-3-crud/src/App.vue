347d196e90e711c484b6003da8ec2f4be6431e16
Pecze Dávid <peczedavid@gmail.com>
2022-08-01 16:15:56 +0200

    Added:
    - Bootstrap 5
    - VSCode extensions in README

--- Frontend/vue-3-crud/src/App.vue
+++ Frontend/vue-3-crud/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>Hello world</h1>
+  <div class="container">
+    <h1>Hello world</h1>
+  </div>
+  
 </template>
 
 <script>
@@ -12,12 +15,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
