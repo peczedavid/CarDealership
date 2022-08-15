1582fe113fc6c3959d37cf0cc2564d1d596f17c6
Pecze Dávid <peczedavid@gmail.com>
2022-08-15 12:26:27 +0200

    Working dashboard sorting

--- Frontend/vue-router-project/src/components/AdminDashboardComponent.vue
+++ Frontend/vue-router-project/src/components/AdminDashboardComponent.vue
@@ -2,10 +2,15 @@
     <table class="table table-striped table-bordered">
         <thead style="background-color: #b2b7df;">
             <tr>
-                <th v-for="userProperty in userProperties" :key="userProperty[0]">
+                <th v-for="(userProperty, index) in userProperties" :key="userProperty[0]">
                     {{ userProperty }}
-                    <button class="float-end bg-transparent border-0"><fa icon="arrow-down"></fa></button>
-                    <button class="float-end bg-transparent border-0"><fa icon="arrow-up"></fa></button>
+                    <button v-if="sortAsc" @click="refreshTable(false, index)"
+                        class="float-end bg-transparent border-0">
+                        <fa icon="arrow-down"></fa>
+                    </button>
+                    <button v-else @click="refreshTable(true, index)" class="float-end bg-transparent border-0">
+                        <fa icon="arrow-up"></fa>
+                    </button>
                 </th>
             </tr>
         </thead>
@@ -26,13 +31,40 @@ export default {
         return {
             users: [{}],
             userProperties: [""],
+            sortByIndex: 0, // id
+            sortAsc: true
         }
+    },
+    methods: {
+        refreshTable(ascending, colIndex) {
+            this.sortAsc = ascending;
+            this.sortByIndex = colIndex;
+            let url = "/user/paging/0/15/";
+
+            url = url + this.userProperties[this.sortByIndex];
+            url = url + "?direction=" + (this.sortAsc ? "asc" : "desc");
+            axios
+                .get(url)
+                .then((result) => {
+                    this.users = result.data.content.map(user => {
+                        return {
+                            id: user.id,
+                            username: user.username,
+                            region: user.region.name,
+                            admin: user.admin
+                        }
+                    });
+                })
+                .catch((error) => {
+                    console.log("Error: ", error);
+                });
+        },
     },
     created() {
         axios
-            .get("/user/all")
+            .get("/user/paging/0/15/id")
             .then((result) => {
-                this.users = result.data.map(user => {
+                this.users = result.data.content.map(user => {
                     return {
                         id: user.id,
                         username: user.username,
@@ -43,7 +75,7 @@ export default {
                 this.userProperties = Reflect.ownKeys(this.users[0]);
             })
             .catch((error) => {
-                console.log(error);
+                console.log("Error: ", error);
             });
     }
 }
